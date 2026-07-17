e236ad9b31308954f67dd6b6c91d52a3332864c4
mohammadAbdulMouty <mabdlmouty@gmail.com>
2026-07-17 03:28:13 +0300

    Release 1.0.0

--- preload.js
+++ preload.js
@@ -1,1 +1,12 @@
 console.log("PRELOAD LOADED");
+const electron = require('electron')
+electron.contextBridge.exposeInMainWorld("electron",{
+    updater: {
+        onStatus: (callback: (data: any) => void) => {
+            electron.ipcRenderer.on("update-status", (event, data) => {
+                console.log(data);
+                callback(data);
+            });
+        }
+    }
+});
--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('initial')
+window.electron.updater.onStatus((data) => {
+
+    console.log(data);
+
+    const statusElement = document.getElementById("update-status");
+
+    if (data.status === "checking") {
+        statusElement.innerText = "Checking for updates...";
+    }
+
+    if (data.status === "available") {
+        statusElement.innerText =
+            `New version ${data.version} available`;
+    }
+
+    if (data.status === "downloading") {
+        statusElement.innerText =
+            `Downloading ${data.percent}%`;
+    }
+
+    if (data.status === "downloaded") {
+        statusElement.innerText =
+            "Update ready. Restart application";
+    }
+
+    if (data.status === "error") {
+        statusElement.innerText =
+            data.message;
+    }
+
+});
